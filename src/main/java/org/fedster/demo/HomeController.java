7b9135ab313e4399d8a918d829c60c966a956f55
Federico Carbonell <fed.carbonell@gmail.com>
2019-03-11 13:34:02 -0400

    Made changes to HomeController to include RequestMapping. Changed title.

--- src/main/java/org/fedster/demo/HomeController.java
+++ src/main/java/org/fedster/demo/HomeController.java
@@ -1,9 +1,11 @@
 package org.fedster.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
     public String homePage() {
         return "index";
     }
